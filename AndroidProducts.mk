867e8dbd0cf0f980c4c35df269c26c9ff0a334dd
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-29 11:44:48 +0100

    TECNO-CI6: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO CI6 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CI6.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CI6-user \
+    omni_TECNO-CI6-userdebug \
+    omni_TECNO-CI6-eng
